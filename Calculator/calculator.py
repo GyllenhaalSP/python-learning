7065a16c30b8802e678c7f9d21db9a972555035d
GyllenhaalSP <86900724+GyllenhaalSP@users.noreply.github.com>
2022-07-30 02:18:54 +0200

    Modified the art.py to reflect new operations available. Added modulus, exponent and percentage operations. Minor cosmetic changes. Added text explaining the new % mechanic. Changed result display to adapt to % operations. Added the new operations to symbol check dict.
    Signed-off-by: GyllenhaalSP <86900724+GyllenhaalSP@users.noreply.github.com>

--- Calculator/calculator.py
+++ Calculator/calculator.py
@@ -85,6 +85,60 @@ def divide(n1, n2):
     return n1 / n2
 
 
+def modulus(n1, n2):
+    """
+    Returns the remainder of the division of n1 by n2, checking and converting types accordingly.
+    """
+    if type(n1) is str:
+        if '.' in n1:
+            n1 = float(n1)
+        else:
+            n1 = int(n1)
+
+    if type(n2) is str:
+        if '.' in n2:
+            n2 = float(n2)
+        else:
+            n2 = int(n2)
+    return n1 % n2
+
+
+def exponent(n1, n2):
+    """
+    Raises n1 to the potency of n2, checking their type and converting them accordingly.
+    """
+    if type(n1) is str:
+        if '.' in n1:
+            n1 = float(n1)
+        else:
+            n1 = int(n1)
+
+    if type(n2) is str:
+        if '.' in n2:
+            n2 = float(n2)
+        else:
+            n2 = int(n2)
+    return n1 ** n2
+
+
+def percentage(n1, n2):
+    """
+    Returns the n2 percentage of n1, checking their type and converting them accordingly.
+    """
+    if type(n1) is str:
+        if '.' in n1:
+            n1 = float(n1)
+        else:
+            n1 = int(n1)
+
+    if type(n2) is str:
+        if '.' in n2:
+            n2 = float(n2)
+        else:
+            n2 = int(n2)
+    return n2 * (n1 / 100)
+
+
 def menu_formatting():
     """
     Formats the output of the 'for' loop containing the operation symbols.
@@ -101,8 +155,10 @@ def calculator():
     Doesn't take any arguments. Simply used for recursion.
     """
     clear()
-    print(f'{" CALCULATOR ".center(104, "*")}')
+    print(f'{" CALCULATOR ".center(110, "*")}')
     print(header)
+    print('For percentages, the first number that it is introduced is the percentage.\n')
+
     num1 = input('What\'s the first number?: ')
     flag = True
     while flag:
@@ -132,7 +188,12 @@ def calculator():
                 num2 = input('Invalid input. Please enter a number: ')
     calculation = symbols[operation]
     result = calculation(num1, num2)
-    print(f'\n{num1} {operation} {num2} = {result}\n')
+    if operation == '%':
+        print(f'\n{num1} {operation} {num2} = {result}')
+        print(f'\n{num1} {operation} {num2} = {num2} + {num1}% ({result}) = {result + float(num2)}')
+        print(f'\n{num1} {operation} {num2} = {num2} - {num1}% ({result}) = {float(num2) - result}\n')
+    else:
+        print(f'\n{num1} {operation} {num2} = {result}\n')
     if input(f'Do you want to keep calculating with {result}?\n"y" or "n": ') == 'n':
         print(f'\nCalculation result is {result}\n')
         while True:
@@ -161,7 +222,12 @@ def calculator():
             print('')
             calculation = symbols[operation]
             result2 = calculation(result, num3)
-            print(f'{result} {operation} {num3} = {result2}\n')
+            if operation == '%':
+                print(f'\n{result} {operation} {num3} = {result2}')
+                print(f'\n{result} {operation} {num3} = {result2} + {result}% ({result2}) = {result2 + float(num2)}')
+                print(f'\n{result} {operation} {num3} = {num3} - {result}% ({result2}) = {float(num3) - result2}\n')
+            else:
+                print(f'\n{result} {operation} {num3} = {result2}\n')
             result = result2
             if input(f'Do you want to keep calculating with {result2}?\n"y" or "n": ') == 'y':
                 print('')
@@ -182,6 +248,9 @@ symbols = {
         '-':   subtract,
         '*':   multiply,
         '/':   divide,
+        'mod': modulus,
+        '^':   exponent,
+        '%':   percentage,
 }
 
 operations = ['+', '-', '*', '/', 'mod', '^', '%']
