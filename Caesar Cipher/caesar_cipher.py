fe4199fa258a09fad6345d390f5ae09b93e748e5
GyllenhaalSP <86900724+GyllenhaalSP@users.noreply.github.com>
2022-08-08 01:55:27 +0200

    Added type hinting to all functions. get_lang() reduced to a one-liner ternary expression. New language_matching function that makes use of the new match-case statement just for the lulz and to see how it actually worked in a "real" environment (albeit a pretty inconsequential one). New text_direction function that takes a bit of code out of the main ones. Polished caesar(): changed lang variable, changed direction tuples to sets, Separated program() into a spanish and english separated function. Renamed language variable again. New restart function. Hoisted that bit of repeated code into its own function. New main function to check language and call the language version of the program.
    Signed-off-by: GyllenhaalSP <86900724+GyllenhaalSP@users.noreply.github.com>

--- Caesar Cipher/caesar_cipher.py
+++ Caesar Cipher/caesar_cipher.py
@@ -8,7 +8,7 @@ from art import header_en, header_es
 
 def clear():
     """
-    Clear the console window
+    Clear the console window.
     """
     if os.name == 'nt':
         _ = os.system('cls')
@@ -16,17 +16,56 @@ def clear():
         _ = os.system('clear')
 
 
-def get_lang():
+def get_lang() -> str:
     """
-    Gets the user OS language
+    Gets the user OS language.
+    Returns the first item in the (locale, codification) tuple returned by getdefaultlocale().
     """
-    if os.name != 'posix':
-        return locale.getdefaultlocale()
-    elif os.name == 'posix':
-        return os.getenv('LANG')
+    lang = locale.getdefaultlocale() if os.name != 'posix' else os.getenv('LANG')
+    return lang[0]
 
 
-def caesar(lang, message, shift_amount, cipher_direction):
+def language_matching(lang: str) -> str:
+    """
+    Returns lang variable according to the results of the get_lang function.
+    """
+    match lang:
+        case 'en_GB' | 'en_US':
+            lang = 'en'
+            print(header_en)
+            return lang
+        case 'es_ES':
+            lang = 'es'
+            print(header_es)
+            return lang
+        case _:
+            print('Your language seems to be neither English nor Spanish. Defaulting to English.')
+            lang = 'en'
+            clear()
+            print(header_en)
+            return lang
+
+
+def text_direction(language: str) -> str:
+    """
+    Returns user choice regarding ciphering direction in both languages.
+    """
+    while True:
+        if language in 'es':
+            direction = input("Introduce 'codificar' para encriptar o 'descodificar' para desencriptar: ").lower()
+            if direction in ('codificar', 'cod', 'c', 'descodificar', 'desc', 'd'):
+                return direction
+            print('\nOpción no encontrada.\n')
+            continue
+        elif language in 'en':
+            direction = input("Type 'encode' to encrypt, type 'decode' to decrypt: ").lower()
+            if direction in ('encode', 'enc', 'e', 'decode', 'dec', 'd'):
+                return direction
+            print('\nNo such option.\n')
+            continue
+
+
+def caesar(language: str, message: str, shift_amount: int, cipher_direction: str) -> str:
     """
     Receives the message to cipher and applies the shift_amount and cipher_direction chosen by the user.
     """
@@ -36,18 +75,18 @@ def caesar(lang, message, shift_amount, cipher_direction):
                'q', 'r', 's', 't', 'u', 'ú', 'ü', 'v', 'w', 'x', 'y', 'z']
     rejoined = []
     alphabet_len = 0
-    split = [char for char in message]
+    split = list(message)
 
-    if cipher_direction in ('decode', 'dec', 'd', 'descodificar', 'des'):
+    if cipher_direction in {'decode', 'dec', 'd', 'descodificar', 'des'}:
         shift_amount *= -1
 
-    if lang in 'es':
+    if language in 'es':
         alphabet_len = len(alph_es)
-    elif lang in 'en':
+    elif language in 'en':
         alphabet_len = len(alph_en)
 
     for char in split:
-        if lang in 'es':
+        if language in 'es':
             if char not in alph_es:
                 rejoined.append(char)
                 continue
@@ -57,7 +96,7 @@ def caesar(lang, message, shift_amount, cipher_direction):
             elif index < 0:
                 index += alphabet_len
             rejoined.append(alph_es[index])
-        elif lang in 'en':
+        elif language in 'en':
             if char not in alph_en:
                 rejoined.append(char)
                 continue
@@ -68,102 +107,96 @@ def caesar(lang, message, shift_amount, cipher_direction):
                 index += alphabet_len
             rejoined.append(alph_en[index])
     cipher_text = ''.join(rejoined)
-    if cipher_direction in ('encode', 'enc', 'e', 'codificar', 'cod', 'c'):
-        if lang in 'es':
+
+    if cipher_direction in {'encode', 'enc', 'e', 'codificar', 'cod', 'c'}:
+        if language in 'es':
             return f'\nEl texto codificado es: {cipher_text.capitalize()}\n'
-        elif lang in 'en':
+        elif language in 'en':
             return f'\nThe encoded text is: {cipher_text.capitalize()}\n'
-    elif cipher_direction in ('decode', 'dec', 'd', 'descodificar', 'des'):
-        if lang in 'es':
+
+    elif cipher_direction in {'decode', 'dec', 'd', 'descodificar', 'des'}:
+        if language in 'es':
             return f'\nEl texto descodificado es: {cipher_text.capitalize()}\n'
-        elif lang in 'en':
+        elif language in 'en':
             return f'\nThe decoded text is: {cipher_text.capitalize()}\n'
 
 
-def program():
+def restart(language: str):
     """
-    Main program wrapped in a function for recursion purposes.
+    Restart the game.
     """
-    lang = get_lang()
-    lang = lang[0]
+    if language in 'es':
+        choice = input('¿Quieres volver a encriptar o desencriptar un mensaje? Teclea "Sí" o "No": ').lower()
+        if choice in ('si', 'sí', 's'):
+            clear()
+            program_es(language)
+        else:
+            print('\n¡Adiós!')
+            quit()
+    elif language in 'en':
+        choice = input('Do you want to go again? Type "Yes" or "No": ').lower()
+        if choice in ('yes', 'y'):
+            clear()
+            program_en(language)
+        else:
+            print('\nGoodbye')
+            quit()
 
-    if lang in ('en_GB', 'en_US'):
-        lang = 'en'
-        print(header_en)
-    elif lang in 'es_ES':
-        lang = 'es'
-        print(header_es)
-    else:
-        print('Your language seems to be neither English nor Spanish. Defaulting to English.')
-        lang = 'en'
-        clear()
-        print(header_en)
+
+def program_es(language: str):
+    """
+    Spanish version of the main program.
+    """
+    direction = text_direction(language)
+    text = input("\nIntroduce el mensaje: ").lower()
+    shift = input("Introduce el número de espacios de desplazamiento: ")
+    while True:
+        if shift.isdigit():
+            shift = int(shift)
+            if shift > 30:
+                shift %= 31
+            break
+        else:
+            shift = input('\nInput inválido. Por favor, introduce un número: ')
+            continue
+
+    print(caesar(language, text, shift, direction))
+
+    restart(language)
+
+
+def program_en(language: str):
+    """
+    English version of the main program.
+    """
+    direction = text_direction(language)
+    text = input("\nType your message: ").lower()
+    shift = input("Type the shift number: ")
+    while True:
+        if shift.isdigit():
+            shift = int(shift)
+            if shift > 25:
+                shift %= 26
+            break
+        else:
+            shift = input('\nInvalid input. Please introduce a number: ')
+            continue
+
+    print(caesar(language, text, shift, direction))
+
+    restart(language)
+
+
+def main():
+    """
+    Main function. Checks user language and calls the correct version of the game.
+    """
+    lang = language_matching(get_lang())
 
     if lang in 'es':
-        while True:
-            direction = input("Introduce 'codificar' para encriptar o 'descodificar' para desencriptar: ").lower()
-            if direction in ('codificar', 'cod', 'c'):
-                break
-            elif direction in ('descodificar', 'desc', 'd'):
-                break
-            else:
-                print('\nOpción no encontrada.')
-                continue
-
-        text = input("Introduce el mensaje: ").lower()
-        shift = input("Introduce el número de espacios de desplazamiento: ")
-        while True:
-            if shift.isdigit():
-                shift = int(shift)
-                if shift > 30:
-                    shift %= 31
-                break
-            else:
-                shift = input('\nInput inválido. Por favor, introduce un número: ')
-                continue
-
-        print(caesar(lang, text, shift, direction))
-
-        restart = input('¿Quieres volver a encriptar o desencriptar un mensaje? Teclea "Sí" o "No": ').lower()
-        if restart in ('si', 'sí', 's'):
-            clear()
-            program()
-        else:
-            print('¡Adiós!')
-            quit()
-
+        program_es(lang)
     elif lang in 'en':
-        while True:
-            direction = input("Type 'encode' to encrypt, type 'decode' to decrypt: ").lower()
-            if direction in ('encode', 'enc', 'e'):
-                break
-            elif direction in ('decode', 'dec', 'd'):
-                break
-            else:
-                print('\nNo such option')
-                continue
-
-        text = input("Type your message: ").lower()
-        shift = input("Type the shift number: ")
-        while True:
-            if shift.isdigit():
-                shift = int(shift)
-                if shift > 25:
-                    shift %= 26
-                break
-            else:
-                shift = input('\nInvalid input. Please introduce a number: ')
-                continue
-
-        print(caesar(lang, text, shift, direction))
-
-        restart = input('Do you want to go again? Type "Yes" or "No": ')
-        if restart in ('yes', 'y'):
-            clear()
-            program()
-        else:
-            print('Goodbye')
-            quit()
+        program_en(lang)
 
 
-program()
+main()
