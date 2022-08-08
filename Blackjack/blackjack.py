e1d0866f7fa1f36df5ba2f637afaa8038eaa8bcb
GyllenhaalSP <86900724+GyllenhaalSP@users.noreply.github.com>
2022-08-08 23:44:24 +0200

    :recycle: Added type hinting. Definitive version of clear(), mini as it can get. Converted for loop into list comprehension in deal_card(). Minor changes here and there to docstrings. Hoisted a bit of code from main function into new computer_hand_deal(), to deal the computer cards automatically. Simplified main while loop. Separated the file with if-main.
    Signed-off-by: GyllenhaalSP <86900724+GyllenhaalSP@users.noreply.github.com>

--- Blackjack/blackjack.py
+++ Blackjack/blackjack.py
@@ -10,25 +10,23 @@ def clear():
     """
     Clear the console window.
     """
-    if os.name == 'nt':
-        _ = os.system('cls')
-    elif os.name == 'posix':
-        _ = os.system('clear')
+    os.system('cls||clear') if os.name in ('nt', 'posix') else print('\n'*100)
 
 
-def deal_card(num_of_cards, hand_list):
+def deal_card(num_of_cards: int, hand_list: list) -> list:
     """
-    Returns wrapped inside an already existing list the passed number of random cards in the deck.
+    Returns, wrapped inside an already existing list, the passed number of random cards in the deck.
     """
     cards = (11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10)
-    for _ in range(num_of_cards):
-        hand_list.append(cards[random.randint(0, len(cards) - 1)])
+    hand_list.extend(cards[random.randint(0, len(cards) - 1)] for _ in range(num_of_cards))
+
     return hand_list
 
 
-def score(hand_list):
+def score(hand_list: list) -> int:
     """
-    Take a list of cards and return the calculated total score of the cards.
+    Take a list of cards and return the total score of the cards. If the user has blackjack in the first
+    hand, it returns 0 to flag blackjack.
     """
     score_sum = sum(hand_list)
     if score_sum == 21 and len(hand_list) == 2:
@@ -39,13 +37,23 @@ def score(hand_list):
     return sum(hand_list)
 
 
-def compare(user_score, computer_score):
+def computer_hand_deal(computer_hand: list, computer_score: int) -> (list, int):
+    """
+    Keeps dealing cards to the computer until the conditions are met, so the values are sent for comparison.
+    """
+    while computer_score != 0 and computer_score < 17:
+        computer_hand = deal_card(1, computer_hand)
+        computer_score = score(computer_hand)
+    return computer_hand, computer_score
+
+
+def compare(user_score: int, computer_score: int) -> str:
     """
     Compares the scores and returns the appropriate win/lose/draw statement.
     """
     if user_score == computer_score:
         return 'It\'s a draw!\n'
-    elif computer_score == 0:
+    elif computer_score == 21:
         return 'You lose! Computer has Blackjack!\n'
     elif user_score == 0:
         return 'You win! You have Blackjack!\n'
@@ -61,7 +69,7 @@ def compare(user_score, computer_score):
 
 def game():
     """
-    Main chunk of the game's code inside a function for recursion.
+    Main function for recursion.
     """
     print(header)
 
@@ -71,24 +79,19 @@ def game():
     deal_card(2, computer_hand)
     computer_score = score(computer_hand)
     user_score = score(user_hand)
-    game_over = False
 
-    while not game_over:
+    while True:
         print(f'\nYour cards: {user_hand}, current score: {user_score}')
         print(f'Computer\'s first card: {computer_hand[0]}\n')
         if user_score == 0 or computer_score == 0 or user_score > 21:
-            game_over = True
-        else:
-            user_wants_deal = input('   Type "deal" to get another card or type "pass" to pass: ')
-            if user_wants_deal in ('deal', 'd'):
-                user_hand = deal_card(1, user_hand)
-                user_score = score(user_hand)
-            else:
-                game_over = True
+            break
+        user_wants_deal = input('   Type "deal" to get another card or type "pass" to pass: ')
+        if user_wants_deal not in ('deal', 'd'):
+            break
+        user_hand = deal_card(1, user_hand)
+        user_score = score(user_hand)
 
-    while computer_score != 0 and computer_score < 17:
-        computer_hand = deal_card(1, computer_hand)
-        computer_score = score(computer_hand)
+    computer_score, computer_hand = computer_hand_deal(computer_hand, computer_score)
 
     print(f'\nYour final hand: {user_hand}, final score {user_score}')
     print(f'Computer\'s final hand: {computer_hand}, final score {computer_score}\n')
@@ -100,5 +103,7 @@ def game():
     quit()
 
 
-clear()
-game()
+if __name__ == '__main__':
+
+    clear()
+    game()
