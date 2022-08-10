e9511e5cd4e9258e7d183074738414d7b25d4dac
GyllenhaalSP <86900724+GyllenhaalSP@users.noreply.github.com>
2022-08-10 21:22:17 +0200

    :art:Another overhaul. I keep refactoring the code as much as possible, correcting redundant code or changing the approach to something with the new things I learn. Added functionality for an "edge"-case in which there are 2 or more bidders with the same name which happen to also have bid for the same amount.
    Signed-off-by: GyllenhaalSP <86900724+GyllenhaalSP@users.noreply.github.com>

--- Blind Auction/blind_auction.py
+++ Blind Auction/blind_auction.py
@@ -9,36 +9,55 @@ def clear():
     """
     Clear the console window.
     """
-    if os.name == 'nt':
-        _ = os.system('cls')
-    elif os.name == 'posix':
-        _ = os.system('clear')
+    os.system('cls||clear') if os.name in ('nt', 'posix') else print('\n'*100)
 
 
-def bidders_info_dict(bidder_name, bidder_price):
+def print_header():
+    """
+    Prints the header where needed.
+    """
+    print(header)
+    print(' Welcome to the Secret Auction Program '.center(108, '*'), '\n')
+
+
+def bidders_info_dict(bidder_name: str, bidder_price: int):
     """
     Updates dictionary populating it with the data the user inputs.
     """
+    if bidder_name in data_dict.keys():
+        bidder_name_numeric_augment = f'{bidder_name} {str(0) if bidders_info_dict.counter < 10 else ""}' \
+                                      f'{str(bidders_info_dict.counter)}'
+        data_dict.update({bidder_name_numeric_augment: bidder_price})
+        bidders_info_dict.counter += 1
     data_dict.update({bidder_name: bidder_price})
 
 
-def find_highest(info_dict):
+def find_highest(info_dict: dict) -> list:
     """
     Find and returns the passed dictionary's highest value by key.
     """
-    return max(info_dict, key=info_dict.get)
+    return [k for k, v in info_dict.items() if v == max(info_dict.values())]
 
-
-print(header)
-print(' Welcome to the Secret Auction Program '.center(108, '*'), '\n')
 
 data_dict = {}
+bidders_info_dict.counter = 0
 
 while True:
+    print_header()
     name = input('What is your name?: ')
-    bid = int(input('What is your bid?: '))
-    more_bidders = input('Are there any other bidders? Type "yes" or "no".\n')
+    while True:
+        bid = input('What is your bid?: ')
+        if bid.isdigit():
+            break
+        print(f'Introduce a numeric bid. Not {bid}.')
+    bid = int(bid)
     bidders_info_dict(name, bid)
+    while True:
+        more_bidders = input('Are there any other bidders? Type "yes" or "no".\n')
+        if more_bidders in ('yes', 'y', 'no', 'n'):
+            break
+        print(f'Invalid input: {more_bidders}')
+
     if more_bidders in ('yes', 'y'):
         clear()
         continue
@@ -47,5 +66,9 @@ while True:
         break
 
 highest_bidder = find_highest(data_dict)
-
-print(f'{highest_bidder} is the highest bidder with {data_dict[highest_bidder]} €!')
+print_header()
+if len(highest_bidder) == 1:
+    print(f'{"".join(highest_bidder)} is the highest bidder with {data_dict[highest_bidder[0]]}€!')
+else:
+    print(f'{", ".join(list(highest_bidder[:-1]))} and {highest_bidder[-1]} are the highest bidders with '
+          f'{data_dict[highest_bidder[0]]}€!')
