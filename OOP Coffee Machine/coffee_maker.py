fa28e0c22c806dc5d73d86343d821bbfb29c8e6c
GyllenhaalSP <86900724+GyllenhaalSP@users.noreply.github.com>
2022-08-18 16:31:59 +0200

    :recycle: Code refactoring and enhancing.
    Updated docstrings. Added type hinting. Refactored is_resource_sufficient() return values.
    
    Signed-off-by: GyllenhaalSP <86900724+GyllenhaalSP@users.noreply.github.com>

--- OOP Coffee Machine/coffee_maker.py
+++ OOP Coffee Machine/coffee_maker.py
@@ -1,10 +1,13 @@
 """
-Predefined classes for the OOP version of the Coffee Machine Program. By APPBrewery.
+Predefined classes for the OOP version of the Coffee Machine Program. By APPBrewery. Modified by GyllenhaalSP.
 """
+from menu import MenuItem
 
 
 class CoffeeMaker:
-    """Models the machine that makes the coffee"""
+    """
+    Models the machine that makes the coffee
+    """
     def __init__(self):
         self.resources = {
             "water": 300,
@@ -12,23 +15,28 @@ class CoffeeMaker:
             "coffee": 100,
         }
 
-    def report(self):
-        """Prints a report of all resources."""
+    def report(self) -> None:
+        """
+        Prints a report of all resources.
+        """
         print(f"Water: {self.resources['water']}ml")
         print(f"Milk: {self.resources['milk']}ml")
         print(f"Coffee: {self.resources['coffee']}g")
 
-    def is_resource_sufficient(self, drink):
-        """Returns True when order can be made, False if ingredients are insufficient."""
-        can_make = True
+    def is_resource_sufficient(self, drink: MenuItem) -> bool:
+        """
+        Returns True when the order can be crafted, False if ingredients are insufficient.
+        """
         for item in drink.ingredients:
             if drink.ingredients[item] > self.resources[item]:
                 print(f"Sorry there is not enough {item}.")
-                can_make = False
-        return can_make
+                return False
+        return True
 
-    def make_coffee(self, order):
-        """Deducts the required ingredients from the resources."""
+    def make_coffee(self, order: MenuItem) -> None:
+        """
+        Deducts the required ingredients from the machine resources and gives the user their beverage.
+        """
         for item in order.ingredients:
             self.resources[item] -= order.ingredients[item]
         print(f"Here is your {order.name} ☕️. Enjoy!\n")
