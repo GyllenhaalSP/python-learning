5cd0ec559198fb6cd22d9bf5b45c94b2122d0118
GyllenhaalSP <86900724+GyllenhaalSP@users.noreply.github.com>
2022-08-18 16:33:23 +0200

    :recycle: Code refactoring and enhancing.
    Updated docstrings. Added type hinting. Refactored get_items() to a one liner. Resolved name conflict: menu -> selector.
    
    Signed-off-by: GyllenhaalSP <86900724+GyllenhaalSP@users.noreply.github.com>

--- OOP Coffee Machine/menu.py
+++ OOP Coffee Machine/menu.py
@@ -1,11 +1,13 @@
 """
-Predefined classes for the OOP version of the Coffee Machine Program. By APPBrewery.
+Predefined classes for the OOP version of the Coffee Machine Program. By APPBrewery. Modified by GyllenhaalSP.
 """
 
 
 class MenuItem:
-    """Models each Menu Item."""
-    def __init__(self, name, water, milk, coffee, cost):
+    """
+    Models each menu item.
+    """
+    def __init__(self, name: str, water: int, milk: int, coffee: int, cost: int | float):
         self.name = name
         self.cost = cost
         self.ingredients = {
@@ -16,27 +18,27 @@ class MenuItem:
 
 
 class Menu:
-    """Models the coffee machine drink selection menu."""
+    """
+    Models the coffee machine drink selection menu.
+    """
     def __init__(self):
-        self.menu = [
+        self.selector = [
             MenuItem(name="latte", water=200, milk=150, coffee=24, cost=2.5),
             MenuItem(name="espresso", water=50, milk=0, coffee=18, cost=1.5),
             MenuItem(name="cappuccino", water=250, milk=50, coffee=24, cost=3),
         ]
 
-    def get_items(self):
-        """Returns all the names of the available menu items"""
-        options = ""
-        for item in self.menu:
-            if item.name == "cappuccino":
-                options += f"{item.name}"
-            else:
-                options += f"{item.name}/"
-        return options
+    def get_items(self) -> str:
+        """
+        Returns all the names of the available menu items.
+        """
+        return "".join(f"{item.name}" if item.name == "cappuccino" else f"{item.name}/" for item in self.selector)
 
-    def find_drink(self, order_name):
-        """Searches the menu for a particular drink by name. Returns that item if it exists, otherwise returns None"""
-        for item in self.menu:
+    def find_drink(self, order_name: str) -> MenuItem | None:
+        """
+        Searches the menu for a particular drink by name. Returns that item if it exists, otherwise returns None.
+        """
+        for item in self.selector:
             if item.name == order_name:
                 return item
         print("Sorry that item is not available.")
