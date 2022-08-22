efee26e351d643f0a8509b272367cd56afe7c407
GyllenhaalSP <86900724+GyllenhaalSP@users.noreply.github.com>
2022-08-22 19:29:12 +0200

    :recycle: Complete overhaul. Trying different things to thin the code more. Thanks to a dear friend for showing me the way :wink:
    Signed-off-by: GyllenhaalSP <86900724+GyllenhaalSP@users.noreply.github.com>

--- Hirst Painting/hirst_painting.py
+++ Hirst Painting/hirst_painting.py
@@ -4,13 +4,13 @@
 import turtle as timmy
 import random
 
+INITIAL_X, INITIAL_Y = (-300, -300)
+DOT_DISTANCE = 60
+N_ROWS = 10
+N_COLS = 20
+
 tim = timmy.Turtle()
 screen = timmy.Screen()
-
-INITIAL_Y = -300
-INITIAL_X = -300
-INCREMENT_Y = 60
-
 screen.setup(2200, 1350)
 screen.title('Hirst Painting')
 tim.shape("turtle")
@@ -18,6 +18,7 @@ tim.color("firebrick1")
 tim.screen.colormode(255)
 tim.speed(0)
 
+# RGB colors from colorama.
 rgb_colors = [(239, 236, 238), (238, 237, 236), (237, 237, 241), (26, 108, 164), (193, 38, 81), (237, 161, 50),
               (234, 215, 86), (227, 237, 229), (223, 137, 176), (143, 108, 57), (103, 197, 219), (21, 57, 132),
               (205, 166, 30), (213, 74, 91), (238, 89, 49), (142, 208, 227), (119, 191, 139), (5, 185, 177),
@@ -25,17 +26,18 @@ rgb_colors = [(239, 236, 238), (238, 237, 236), (237, 237, 241), (26, 108, 164),
               (173, 185, 221), (29, 90, 95), (233, 173, 162), (156, 212, 190), (87, 46, 33), (37, 45, 83)]
 
 
-line_counter = 0
+def pick_random_color():
+    """
+    Returns a random color from color list.
+    """
+    return random.choice(rgb_colors)
+
+
 tim.pu()
-while line_counter <= 9:
-    tim.setpos(INITIAL_X, INITIAL_Y + INCREMENT_Y)
-    for line in range(10):
-        tim.color(random.choice(rgb_colors))
-        tim.dot(30)
-        tim.pu()
-        tim.fd(60)
-    line_counter += 1
-    INCREMENT_Y += 60
+for row in range(N_ROWS):
+    for col in range(N_COLS):
+        tim.setpos(INITIAL_X + col * DOT_DISTANCE, INITIAL_Y + row * DOT_DISTANCE)
+        tim.dot(30, pick_random_color())
 
 tim.ht()
 
