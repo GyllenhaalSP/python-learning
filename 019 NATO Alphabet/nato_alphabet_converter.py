1f004e3e15f2b3638d779ad377d128a3d4cc52e8
GyllenhaalSP <86900724+GyllenhaalSP@users.noreply.github.com>
2022-08-27 14:11:56 +0200

    :recycle: Added functionality.
    Used unidecode module to normalize accents. Used try/except to make sure only numbers are entered. Added loop terminator words.
    
    Signed-off-by: GyllenhaalSP <86900724+GyllenhaalSP@users.noreply.github.com>

--- 019 NATO Alphabet/nato_alphabet_converter.py
+++ 019 NATO Alphabet/nato_alphabet_converter.py
@@ -2,11 +2,16 @@
  By GyllenhaalSP ago. 2022 @ https://github.com/GyllenhaalSP.
 """
 import pandas as pd
+from unidecode import unidecode
 
 nato_dict = {data.letter: data.code for index, data in pd.read_csv('nato_phonetic_alphabet.csv').iterrows()}
 
-while word := input('What word do you want to convert? (empty field to exit): '):
-
-    print([nato_dict[char] for char in word.upper() if char in nato_dict], end='\n\n')
+while word := input('What word do you want to convert?: '):
+    try:
+        if word in {'e', 'end', 's', 'stop', 'q', 'quit'}:
+            break
+        print([nato_dict[char] for char in unidecode(word).upper()], end='\n\n')
+    except KeyError:
+        print('Sorry, only letters in the alphabet, please!\n')
 
 print('\nBye')
